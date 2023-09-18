cc7980b1343a3d663d328fd01d76552123689080
RezoanulHasan <rezoanulhasan96@gmail.com>
2023-09-18 23:47:49 +0600

    add room route by all and email

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ var cors = require('cors');
 require ('dotenv').config();
 //for   mongodb
 const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
-
+//for jwt token
+const jwt = require('jsonwebtoken');
 
 const port = process.env.PORT || 5000;
-const datas = require('./data/rooms.json');
+//const datas = require('./data/rooms.json');
 
 
 // use   middleware
@@ -18,7 +19,23 @@ app.use(express.json());
 app.use(cors());
 
 
+//(verifyJWT)
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
 
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 //  mongodb user and pass
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aatv5yk.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -44,11 +61,37 @@ async function run() {
 
 const contactCollection = client.db('aircnc').collection('contacts');
 const usersCollection = client.db("aircnc").collection("users");
+const roomsCollection = client.db('aircnc').collection('rooms')
+
+
+
+    //*---------------------------using jwt--------------------------*
+
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '7d' })
+      res.send({ token })
+    })
+
+
+     // Warning: use verifyJWT before using ( verifyAdmin)
+     const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== 'admin') {
+        return res.status(403).send({ error: true, message: 'forbidden message' });
+      }
+      next();
+    }
+
+
+    //verifyJWT,verifyAdmin , 
 
 
 //*---------------------------users--------------------------*
 // Get all users
-app.get('/users', async (req, res) => {
+app.get('/users', async  (req, res) => {
   const result = await usersCollection.find().toArray();
   res.send(result);
 });
@@ -72,7 +115,7 @@ app.post('/users', async (req, res) => {
 
 // Verify if a user is an admin
 //verifyJWT
-app.get('/users/admin/:email', async (req, res) => {
+app.get('/users/admin/:email',  verifyJWT, async (req, res) => {
   const email = req.params.email;
   if (req.decoded.email !== email) {
     res.send({ admin: false });
@@ -104,7 +147,7 @@ app.patch('/users/admin/:id', async (req, res) => {
 
 
 // Verify if a user is an host
-app.get('/users/host/:email',  async (req, res) => {
+app.get('/users/host/:email', verifyJWT,  async (req, res) => {
   const email = req.params.email;
   if (req.decoded.email !== email) {
     res.send({ host: false });
@@ -141,6 +184,56 @@ app.delete('/users/:id', async (req, res) => {
 });
 
 
+
+
+
+//*---------------------------rooms--------------------------*
+
+
+  // Get a single room by id
+  app.get('/rooms/:id', async (req, res) => {
+    const id = req.params.id
+    const query = { _id: new ObjectId(id) }
+    const result = await roomsCollection.findOne(query)
+    res.send(result)
+  })
+
+
+
+
+  // Get all rooms
+ // app.get('/rooms', async (req, res) => {
+    //const result = await roomsCollection.find().toArray()
+    //res.send(result)
+ // })
+
+
+
+  // SHOW rooms  data by login user
+  app.get('/rooms',  async (req, res) => {
+    let query = {};
+if (req.query?.email) {
+      query = { email: req.query.email }
+    }
+    const result = await roomsCollection.find(query).toArray();
+    res.send(result);
+})
+
+
+  // Save a room in database
+  app.post('/rooms', async (req, res) => {
+    console.log(req.decoded)
+    const room = req.body
+    const result = await roomsCollection.insertOne(room)
+    res.send(result)
+  })
+  // delete room
+  app.delete('/rooms/:id', async (req, res) => {
+    const id = req.params.id
+    const query = { _id: new ObjectId(id) }
+    const result = await roomsCollection.deleteOne(query)
+    res.send(result)
+  })
 
 
 //*---------------------------contacts--------------------------*
@@ -190,17 +283,17 @@ run().catch(console.dir);app.get('/', (req, res) => {
 
 
 
-app.get('/rooms', (req, res) => {
-    res.send(datas);
-})
+///app.get('/rooms', (req, res) => {
+   // res.send(datas);
+//})
 
 
 
-app.get('/rooms/:id', (req, res) => {
-    const {id} = req.params;
-    const selectedData = datas.find(n => n.id ==id) || {} ;
-    res.send(selectedData);
-})
+//app.get('/rooms/:id', (req, res) => {
+    //const {id} = req.params;
+   // const selectedData = datas.find(n => n.id ==id) || {} ;
+   // res.send(selectedData);
+//})
 
 
 
